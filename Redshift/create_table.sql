2edb8dca6002c83055ba006aac83119513c6d4e8
unknown <francosterrantino@gmail.com>
2023-11-29 14:48:50 -0300

    Se incorpora script de creación de tabla

---
+++ Redshift/create_table.sql
@@ -0,0 +1,11 @@
+create table if not exists stocks_data (
+  ticker VARCHAR(4),
+  volume FLOAT,
+  volume_weighted_avg_price FLOAT,
+  open_price FLOAT,
+  close_price FLOAT,
+  highest_price FLOAT,
+  lowest_price FLOAT,
+  transactions_number FLOAT,
+  datetime FLOAT
+)
