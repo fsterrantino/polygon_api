54418394ca0403ef0365124a1ff851fdf2d03ac6
unknown <francosterrantino@gmail.com>
2023-12-13 14:53:37 -0300

    Se incorporan las modificaciones surgidas de las observaciones del profe: 1) Se excluyen las carpetas __pycache__ en el gitignore, 2) Se crea un archivo de configuración donde se guardan los parámetros variables de las búsquedas a la API, utilizando configparser, 3) Se oculta la api_key en un archivo .env, leyéndola mediante la librería dotenv.

--- poligon_api.py
+++ poligon_api.py
@@ -2,21 +2,27 @@ from scripts.url_params_and_headers import get_url
 from scripts.fetch_data import fetch_data_from_api
 from scripts.parse_response import json_dict_to_dataframe
 from scripts.url_params_and_headers import params
+import configparser
+
+config = configparser.ConfigParser()
+config.sections()
+config.read('./config.ini')
 
 responses_list = []
 # tickers_to_query_list = ['AAPL']
-tickers_to_query_list = ['AAPL', 'AMZN', 'TSLA', 'BABA', 'MSFT']
-start_date = '2023-11-27'
-end_date = '2023-11-27'
-time_frame = 'hour'
-frame_multiplier = 1
+tickers_to_query_list = config['API_PARAMETERS']['tickers_to_query_list']
+tickers_to_query_list = tickers_to_query_list.split(', ')
+start_date = config['API_PARAMETERS']['start_date']
+end_date = config['API_PARAMETERS']['end_date']
+time_frame = config['API_PARAMETERS']['time_frame']
+frame_multiplier = config['API_PARAMETERS']['frame_multiplier']
 
 for ticker in tickers_to_query_list:
     url = None
     while True:
         if not url:
             url = get_url(ticker, start_date, end_date, time_frame, frame_multiplier)
-
+            print(url)
         response = fetch_data_from_api(url, params)
         print(response)
 
--- scripts/url_params_and_headers.py
+++ scripts/url_params_and_headers.py
@@ -1,14 +1,22 @@
-from .api_secrets import api_key
+import configparser
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='./.env')
+API_KEY = os.getenv('API_KEY')
+
+config = configparser.ConfigParser()
+config.sections()
+config.read('./config.ini')
 
 headers = {
-    'Authorization': f'Bearer {api_key}'
+    'Authorization': f'Bearer {API_KEY}'
 }
 
 params = {
     'adjusted': 'true'
     , 'sort': 'asc'
-    , 'limit': 120
-
+    , 'limit': 50000
 }
 
 def get_url(ticker, start_date, end_date, time_frame, frame_multiplier):
@@ -18,7 +26,7 @@ def get_url(ticker, start_date, end_date, time_frame, frame_multiplier):
     url_appending_params = {
         'ticker': ticker
         , 'range': 'range'
-        , 'multiplier': frame_multiplier
+        , 'multiplier': str(frame_multiplier)
         , 'timespam': time_frame
         , 'from': start_date # format: 2023-01-09
         , 'to': end_date # format: 2023-01-10
@@ -27,9 +35,9 @@ def get_url(ticker, start_date, end_date, time_frame, frame_multiplier):
     url_appending = ''
     for key, value in url_appending_params.items():
         if key != 'to':
-            url_appending = url_appending + str(value) + '/'
+            url_appending = url_appending + value + '/'
         else:
-            url_appending = url_appending + str(value)
+            url_appending = url_appending + value
 
     final_url = url + url_appending  
     return final_url
