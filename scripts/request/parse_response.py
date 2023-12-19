2df38a17cac7f13af2bd453ab752d8a5062107ba
unknown <francosterrantino@gmail.com>
2023-12-19 20:01:54 -0300

    Se incorpora el script de upsert de datos con control previo de duplicados, se modifica la fecha que se obtiene y se carga al día de ayer porque la información del día puede ser incompleta según el horario en el que se realice la consulta y la api no devuelve información.

--- scripts/request/parse_response.py
+++ scripts/request/parse_response.py
@@ -14,13 +14,13 @@ def json_dict_to_dataframe(json_dicts_list):
                 , 'close_price': ticker['c']
                 , 'highest_price': ticker['h']
                 , 'lowest_price': ticker['l']
-                , 'timestamp': ticker['t']
+                , 'datetime': ticker['t']
                 , 'transactions_number': ticker['n']
             }
 
             tickers_data.append(ticker_data)
 
     df = pd.DataFrame.from_dict(tickers_data)
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+    df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
 
     return df
